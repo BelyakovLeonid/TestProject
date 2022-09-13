1f8e6912d308109f11f5bb4618f14bbbd5cc4893
Belyakov <lenyabelyakov@mail.ru>
2022-09-13 11:40:15 +0300

    Data. Continue implementing network communication.

--- app/src/main/java/com/belyakov/testproject/base/data/remote/di/NetworkModule.kt
+++ app/src/main/java/com/belyakov/testproject/base/data/remote/di/NetworkModule.kt
@@ -1,14 +1,20 @@
 package com.belyakov.testproject.base.data.remote.di
 
 import com.belyakov.testproject.base.data.remote.interceptor.ApiKeyInterceptor
+import com.belyakov.testproject.newsdetail.presentation.NewsDetailViewModel_Factory
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import dagger.multibindings.IntoSet
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.json.Json
 import okhttp3.Interceptor
+import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
+import retrofit2.Converter
 import retrofit2.Retrofit
 import javax.inject.Singleton
 
@@ -21,11 +27,13 @@ class NetworkModule {
     @Provides
     @Singleton
     fun provideRetrofit(
-        client: OkHttpClient
+        client: OkHttpClient,
+        converterFactory: Converter.Factory
     ): Retrofit {
         return Retrofit.Builder()
             .baseUrl(BASE_URL)
             .client(client)
+            .addConverterFactory(converterFactory)
             .build()
     }
 
@@ -36,6 +44,13 @@ class NetworkModule {
         val builder = OkHttpClient.Builder()
         interceptors.forEach { builder.addInterceptor(it) }
         return builder.build()
+    }
+
+    @Provides
+    @OptIn(ExperimentalSerializationApi::class)
+    fun provideConverterFactory(): Converter.Factory {
+        val contentType = "application/json".toMediaType()
+        return Json.asConverterFactory(contentType)
     }
 
     @Module
