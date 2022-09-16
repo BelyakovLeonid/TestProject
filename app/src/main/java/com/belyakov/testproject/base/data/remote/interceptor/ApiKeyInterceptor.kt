ca769e56b298cbc342e4c73d510f1d46878fccf5
Belyakov <lenyabelyakov@mail.ru>
2022-09-16 14:19:19 +0300

    CI/CD Extract API_KEY Create basic CI/CD (which is blocked by the problem with ui tests)

--- app/src/main/java/com/belyakov/testproject/base/data/remote/interceptor/ApiKeyInterceptor.kt
+++ app/src/main/java/com/belyakov/testproject/base/data/remote/interceptor/ApiKeyInterceptor.kt
@@ -1,17 +1,19 @@
 package com.belyakov.testproject.base.data.remote.interceptor
 
+import com.belyakov.testproject.BuildConfig
 import okhttp3.Interceptor
 import okhttp3.Response
 import javax.inject.Inject
 
-class ApiKeyInterceptor @Inject constructor(): Interceptor {
+class ApiKeyInterceptor @Inject constructor() : Interceptor {
 
     override fun intercept(chain: Interceptor.Chain): Response {
         val request = chain.request()
 
         val url = request.url
+
         val newUrl = url.newBuilder()
-            .addQueryParameter(API_KEY_NAME, API_KEY)
+            .addQueryParameter(API_KEY_NAME, BuildConfig.API_KEY)
             .build()
 
         val newRequest = request.newBuilder()
@@ -21,8 +23,7 @@ class ApiKeyInterceptor @Inject constructor(): Interceptor {
         return chain.proceed(newRequest)
     }
 
-    private companion object{
+    private companion object {
         const val API_KEY_NAME = "apiKey"
-        const val API_KEY = "b536d80abd80435284fda380910aebb7" //todo
     }
 }
